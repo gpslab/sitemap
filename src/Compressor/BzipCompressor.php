da30d3b0dce529d3c0625221bc50e924c82d567d
Peter Gribanov <wcode404@gmail.com>
2016-09-19 12:04:12 +0300

    create compressors

---
+++ src/Compressor/BzipCompressor.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * @author    Peter Gribanov <info@peter-gribanov.ru>
+ * @copyright Copyright (c) 2011, Peter Gribanov
+ * @license   http://opensource.org/licenses/MIT
+ */
+namespace GpsLab\Component\Sitemap\Compressor;
+
+class BzipCompressor implements CompressorInterface
+{
+    /**
+     * @param string $source
+     * @param string $target
+     *
+     * @return bool
+     */
+    public function compress($source, $target)
+    {
+        $rh = fopen($source, 'rb');
+        $bz = bzopen($target, 'w9');
+
+        if ($rh === false || $bz === false) {
+            return false;
+        }
+
+        while (!feof($rh)) {
+            if (bzwrite($bz, fread($rh, 1024)) === false) {
+                return false;
+            }
+        }
+
+        fclose($rh);
+        bzclose($bz);
+
+        return true;
+    }
+}
---
+++ src/Compressor/GzipCompressor.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * @author    Peter Gribanov <info@peter-gribanov.ru>
+ * @copyright Copyright (c) 2011, Peter Gribanov
+ * @license   http://opensource.org/licenses/MIT
+ */
+namespace GpsLab\Component\Sitemap\Compressor;
+
+class GzipCompressor implements CompressorInterface
+{
+    /**
+     * @param string $source
+     * @param string $target
+     *
+     * @return bool
+     */
+    public function compress($source, $target)
+    {
+        $rh = fopen($source, 'rb');
+        $gz = gzopen($target, 'w9');
+
+        if ($rh === false || $gz === false) {
+            return false;
+        }
+
+        while (!feof($rh)) {
+            if (gzwrite($gz, fread($rh, 1024)) === false) {
+                return false;
+            }
+        }
+
+        fclose($rh);
+        gzclose($gz);
+
+        return true;
+    }
+}
