4a85127f888b0fc16ef2401c76ef726a64dc3603
Peter Gribanov <wcode404@gmail.com>
2016-09-19 13:48:01 +0300

    create Builder interface and builder collection

---
+++ src/Builder/BuilderInterface.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @author    Peter Gribanov <info@peter-gribanov.ru>
+ * @copyright Copyright (c) 2011, Peter Gribanov
+ * @license   http://opensource.org/licenses/MIT
+ */
+namespace GpsLab\Component\Sitemap\Builder;
+
+use GpsLab\Component\Sitemap\Result\KeeperUriInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+interface BuilderInterface
+{
+    /**
+     * @return string
+     */
+    public function getTitle();
+
+    /**
+     * @param KeeperUriInterface $result
+     * @param SymfonyStyle $io
+     */
+    public function execute(KeeperUriInterface $result, SymfonyStyle $io);
+}
---
+++ src/Builder/CollectionBuilder.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @author    Peter Gribanov <info@peter-gribanov.ru>
+ * @copyright Copyright (c) 2011, Peter Gribanov
+ * @license   http://opensource.org/licenses/MIT
+ */
+namespace GpsLab\Component\Sitemap\Builder;
+
+class CollectionBuilder
+{
+    /**
+     * @var BuilderInterface[]
+     */
+    private $builders = [];
+
+    /**
+     * @param BuilderInterface $builder
+     *
+     * @return self
+     */
+    public function addBuilder(BuilderInterface $builder)
+    {
+        $this->builders[] = $builder;
+
+        return $this;
+    }
+
+    /**
+     * @return BuilderInterface[]
+     */
+    public function getBuilders()
+    {
+        return $this->builders;
+    }
+}
