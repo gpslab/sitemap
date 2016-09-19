6ec7924e2e4656255d445bb3661667772a5f65c1
Peter Gribanov <wcode404@gmail.com>
2016-09-19 14:27:44 +0300

    correct file header comments

--- src/Result/KeeperUriInterface.php
+++ src/Result/KeeperUriInterface.php
@@ -1,10 +1,11 @@
 <?php
 /**
+ * GpsLab component.
+ *
  * @author    Peter Gribanov <info@peter-gribanov.ru>
  * @copyright Copyright (c) 2011, Peter Gribanov
  * @license   http://opensource.org/licenses/MIT
  */
-
 namespace GpsLab\Component\Sitemap\Result;
 
 use GpsLab\Component\Sitemap\Uri\UriInterface;
