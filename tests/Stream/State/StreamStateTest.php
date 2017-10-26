49a005b17d5ddb9ec88448d964ca0b06de5385cb
Peter Gribanov <wcode404@gmail.com>
2017-10-26 16:19:18 +0300

    test StreamState

---
+++ tests/Stream/State/StreamStateTest.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ * GpsLab component.
+ *
+ * @author    Peter Gribanov <info@peter-gribanov.ru>
+ * @copyright Copyright (c) 2011, Peter Gribanov
+ * @license   http://opensource.org/licenses/MIT
+ */
+
+namespace GpsLab\Component\Sitemap\Tests\Stream\State;
+
+use GpsLab\Component\Sitemap\Stream\State\StreamState;
+
+class StreamStateTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var StreamState
+     */
+    private $state;
+
+    protected function setUp()
+    {
+        $this->state = new StreamState();
+    }
+
+    protected function tearDown()
+    {
+        if ($this->state->isReady()) {
+            $this->state->close();
+        }
+    }
+
+    /**
+     * @expectedException \GpsLab\Component\Sitemap\Stream\Exception\StreamStateException
+     */
+    public function testAlreadyOpened()
+    {
+        $this->assertFalse($this->state->isReady());
+        $this->state->open();
+        $this->assertTrue($this->state->isReady());
+
+        // already opened
+        $this->state->open();
+    }
+
+    /**
+     * @expectedException \GpsLab\Component\Sitemap\Stream\Exception\StreamStateException
+     */
+    public function testAlreadyClosed()
+    {
+        $this->assertFalse($this->state->isReady());
+        $this->state->open();
+        $this->assertTrue($this->state->isReady());
+        $this->state->close();
+        $this->assertFalse($this->state->isReady());
+
+        // already closed
+        $this->state->close();
+    }
+
+    /**
+     * @expectedException \GpsLab\Component\Sitemap\Stream\Exception\StreamStateException
+     */
+    public function testNotOpened()
+    {
+        $this->assertFalse($this->state->isReady());
+
+        // not opened
+        $this->state->close();
+    }
+
+    /**
+     * @expectedException \GpsLab\Component\Sitemap\Stream\Exception\StreamStateException
+     */
+    public function testNotClosed()
+    {
+        $state = new StreamState();
+        $state->open();
+        unset($state);
+    }
+
+    public function testAllIsGood()
+    {
+        $state = new StreamState();
+        $this->assertFalse($state->isReady());
+        $state->open();
+        $this->assertTrue($state->isReady());
+        $state->close();
+        $this->assertFalse($state->isReady());
+        unset($state);
+    }
+}
