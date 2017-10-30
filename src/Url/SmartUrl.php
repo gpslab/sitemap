089ba2766d20d3d3a94aea7cde8f43f32ff36946
Peter Gribanov <wcode404@gmail.com>
2017-10-30 16:39:39 +0300

    optimize get change_freq from priority

--- src/Url/SmartUrl.php
+++ src/Url/SmartUrl.php
@@ -12,6 +12,23 @@ namespace GpsLab\Component\Sitemap\Url;
 class SmartUrl extends Url
 {
     /**
+     * @var array
+     */
+    private $change_freq_priority = [
+        '1.0' => self::CHANGE_FREQ_HOURLY,
+        '0.9' => self::CHANGE_FREQ_DAILY,
+        '0.8' => self::CHANGE_FREQ_DAILY,
+        '0.7' => self::CHANGE_FREQ_WEEKLY,
+        '0.6' => self::CHANGE_FREQ_WEEKLY,
+        '0.5' => self::CHANGE_FREQ_WEEKLY,
+        '0.4' => self::CHANGE_FREQ_MONTHLY,
+        '0.3' => self::CHANGE_FREQ_MONTHLY,
+        '0.2' => self::CHANGE_FREQ_YEARLY,
+        '0.1' => self::CHANGE_FREQ_YEARLY,
+        '0.0' => self::CHANGE_FREQ_NEVER,
+    ];
+
+    /**
      * @param string                  $loc
      * @param \DateTimeImmutable|null $last_mod
      * @param string|null             $change_freq
@@ -79,35 +96,14 @@ class SmartUrl extends Url
     /**
      * @param string $priority
      *
-     * @return string
+     * @return string|null
      */
     private function changeFreqFromPriority($priority)
     {
-        switch ($priority) {
-            case '1.0':
-                return self::CHANGE_FREQ_HOURLY;
-            case '0.9':
-                return self::CHANGE_FREQ_DAILY;
-            case '0.8':
-                return self::CHANGE_FREQ_DAILY;
-            case '0.7':
-                return self::CHANGE_FREQ_WEEKLY;
-            case '0.6':
-                return self::CHANGE_FREQ_WEEKLY;
-            case '0.5':
-                return self::CHANGE_FREQ_WEEKLY;
-            case '0.4':
-                return self::CHANGE_FREQ_MONTHLY;
-            case '0.3':
-                return self::CHANGE_FREQ_MONTHLY;
-            case '0.2':
-                return self::CHANGE_FREQ_YEARLY;
-            case '0.1':
-                return self::CHANGE_FREQ_YEARLY;
-            case '0.0':
-                return self::CHANGE_FREQ_NEVER;
-            default:
-                return null;
+        if (isset($this->change_freq_priority[$priority])) {
+            return $this->change_freq_priority[$priority];
         }
+
+        return null;
     }
 }
