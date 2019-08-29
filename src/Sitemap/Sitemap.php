c6319e31f6a31560fac9751248fe4ef06d6d6beb
Peter Gribanov <wcode404@gmail.com>
2019-08-29 16:57:15 +0300

    create sitemap VO

---
+++ src/Sitemap/Sitemap.php
@@ -0,0 +1,54 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * GpsLab component.
+ *
+ * @author    Peter Gribanov <info@peter-gribanov.ru>
+ * @copyright Copyright (c) 2011-2019, Peter Gribanov
+ * @license   http://opensource.org/licenses/MIT
+ */
+
+namespace GpsLab\Component\Sitemap\Sitemap;
+
+/**
+ * The part of sitemap index.
+ */
+class Sitemap
+{
+    /**
+     * @var string
+     */
+    private $location;
+
+    /**
+     * @var \DateTimeInterface|null
+     */
+    private $last_modify;
+
+    /**
+     * @param string                  $location
+     * @param \DateTimeInterface|null $last_modify
+     */
+    public function __construct(string $location, ?\DateTimeInterface $last_modify)
+    {
+        $this->location = $location;
+        $this->last_modify = $last_modify;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLocation(): string
+    {
+        return $this->location;
+    }
+
+    /**
+     * @return \DateTimeInterface|null
+     */
+    public function getLastModify(): ?\DateTimeInterface
+    {
+        return $this->last_modify;
+    }
+}
