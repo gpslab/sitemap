32472d4d520a2b887b09ec4fd2776832142485ef
Peter Gribanov <wcode404@gmail.com>
2019-09-02 17:05:49 +0300

    create Limiter service

---
+++ src/Limiter.php
@@ -0,0 +1,110 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * GpsLab component.
+ *
+ * @author    Peter Gribanov <info@peter-gribanov.ru>
+ * @copyright Copyright (c) 2011-2019, Peter Gribanov
+ * @license   http://opensource.org/licenses/MIT
+ */
+
+namespace GpsLab\Component\Sitemap;
+
+use GpsLab\Component\Sitemap\Stream\Exception\LinksOverflowException;
+use GpsLab\Component\Sitemap\Stream\Exception\SitemapsOverflowException;
+use GpsLab\Component\Sitemap\Stream\Exception\SizeOverflowException;
+
+final class Limiter
+{
+    /**
+     * The maximum number of URLs in a sitemap.
+     */
+    public const LINKS_LIMIT = 50000;
+
+    /**
+     * The maximum number of sitemaps in a sitemap index.
+     */
+    public const SITEMAPS_LIMIT = 50000;
+
+    /**
+     * The maximum size of sitemap.xml in bytes.
+     */
+    public const BYTE_LIMIT = 52428800; // 50 Mb
+
+    /**
+     * @var int
+     */
+    private $added_urls = 0;
+
+    /**
+     * @var int
+     */
+    private $added_sitemaps = 0;
+
+    /**
+     * @var int
+     */
+    private $used_bytes = 0;
+
+    public function tryAddUrl(): void
+    {
+        if ($this->added_urls + 1 > self::LINKS_LIMIT) {
+            throw LinksOverflowException::withLimit(self::LINKS_LIMIT);
+        }
+
+        ++$this->added_urls;
+    }
+
+    /**
+     * @return int
+     */
+    public function howManyUrlsAvailableToAdd(): int
+    {
+        return self::LINKS_LIMIT - $this->added_urls;
+    }
+
+    public function tryAddSitemap(): void
+    {
+        if ($this->added_sitemaps + 1 > self::SITEMAPS_LIMIT) {
+            throw SitemapsOverflowException::withLimit(self::SITEMAPS_LIMIT);
+        }
+
+        ++$this->added_sitemaps;
+    }
+
+    /**
+     * @return int
+     */
+    public function howManySitemapsAvailableToAdd(): int
+    {
+        return self::SITEMAPS_LIMIT - $this->added_sitemaps;
+    }
+
+    /**
+     * @param int $used_bytes
+     */
+    public function tryUseBytes(int $used_bytes): void
+    {
+        if ($this->used_bytes + $used_bytes > self::BYTE_LIMIT) {
+            throw SizeOverflowException::withLimit(self::BYTE_LIMIT);
+        }
+
+        $this->used_bytes += $used_bytes;
+    }
+
+    /**
+     * @return int
+     */
+    public function howManyBytesAvailableToUse(): int
+    {
+        return self::BYTE_LIMIT - $this->used_bytes;
+    }
+
+    public function reset(): void
+    {
+        $this->added_urls = 0;
+        $this->added_sitemaps = 0;
+        $this->used_bytes = 0;
+    }
+}
---
+++ tests/LimiterTest.php
@@ -0,0 +1,107 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * GpsLab component.
+ *
+ * @author    Peter Gribanov <info@peter-gribanov.ru>
+ * @copyright Copyright (c) 2011-2019, Peter Gribanov
+ * @license   http://opensource.org/licenses/MIT
+ */
+
+namespace GpsLab\Component\Sitemap\Tests;
+
+use GpsLab\Component\Sitemap\Limiter;
+use GpsLab\Component\Sitemap\Stream\Exception\LinksOverflowException;
+use GpsLab\Component\Sitemap\Stream\Exception\SitemapsOverflowException;
+use GpsLab\Component\Sitemap\Stream\Exception\SizeOverflowException;
+use PHPUnit\Framework\TestCase;
+
+class LimiterTest extends TestCase
+{
+    /**
+     * @var Limiter
+     */
+    private $limiter;
+
+    protected function setUp(): void
+    {
+        $this->limiter = new Limiter();
+    }
+
+    public function testTryAddUrl(): void
+    {
+        $this->limiter->tryAddUrl();
+        self::assertEquals(Limiter::LINKS_LIMIT - 1, $this->limiter->howManyUrlsAvailableToAdd());
+    }
+
+    public function testTryAddUrlReset(): void
+    {
+        $this->limiter->tryAddUrl();
+        self::assertEquals(Limiter::LINKS_LIMIT - 1, $this->limiter->howManyUrlsAvailableToAdd());
+        $this->limiter->reset();
+        $this->limiter->tryAddUrl();
+        self::assertEquals(Limiter::LINKS_LIMIT - 1, $this->limiter->howManyUrlsAvailableToAdd());
+    }
+
+    public function testTryAddUrlOverflow(): void
+    {
+        for ($i = 0; $i < Limiter::LINKS_LIMIT; ++$i) {
+            $this->limiter->tryAddUrl();
+            self::assertEquals(Limiter::LINKS_LIMIT - ($i + 1), $this->limiter->howManyUrlsAvailableToAdd());
+        }
+
+        $this->expectException(LinksOverflowException::class);
+        $this->limiter->tryAddUrl();
+    }
+
+    public function testTryAddSitemap(): void
+    {
+        $this->limiter->tryAddSitemap();
+        self::assertEquals(Limiter::SITEMAPS_LIMIT - 1, $this->limiter->howManySitemapsAvailableToAdd());
+    }
+
+    public function testTryAddSitemapReset(): void
+    {
+        $this->limiter->tryAddSitemap();
+        self::assertEquals(Limiter::SITEMAPS_LIMIT - 1, $this->limiter->howManySitemapsAvailableToAdd());
+        $this->limiter->reset();
+        $this->limiter->tryAddSitemap();
+        self::assertEquals(Limiter::SITEMAPS_LIMIT - 1, $this->limiter->howManySitemapsAvailableToAdd());
+    }
+
+    public function testTryAddSitemapOverflow(): void
+    {
+        for ($i = 0; $i < Limiter::SITEMAPS_LIMIT; ++$i) {
+            $this->limiter->tryAddSitemap();
+            self::assertEquals(Limiter::SITEMAPS_LIMIT - ($i + 1), $this->limiter->howManySitemapsAvailableToAdd());
+        }
+
+        $this->expectException(SitemapsOverflowException::class);
+        $this->limiter->tryAddSitemap();
+    }
+
+    public function testTryUseBytes(): void
+    {
+        $this->limiter->tryUseBytes(1);
+        self::assertEquals(Limiter::BYTE_LIMIT - 1, $this->limiter->howManyBytesAvailableToUse());
+    }
+
+    public function testTryUseBytesReset(): void
+    {
+        $this->limiter->tryUseBytes(1);
+        self::assertEquals(Limiter::BYTE_LIMIT - 1, $this->limiter->howManyBytesAvailableToUse());
+        $this->limiter->reset();
+        $this->limiter->tryUseBytes(1);
+        self::assertEquals(Limiter::BYTE_LIMIT - 1, $this->limiter->howManyBytesAvailableToUse());
+    }
+
+    public function testTryUseBytesOverflow(): void
+    {
+        $this->limiter->tryUseBytes(Limiter::BYTE_LIMIT);
+        self::assertEquals(0, $this->limiter->howManyBytesAvailableToUse());
+
+        $this->expectException(SizeOverflowException::class);
+        $this->limiter->tryUseBytes(1);
+    }
+}
