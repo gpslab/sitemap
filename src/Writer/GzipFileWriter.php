885a61cfd4be766ab247e5324596bffb6c27edf7
Peter Gribanov <wcode404@gmail.com>
2019-09-02 15:29:01 +0300

    create GzipFileWriter and GzipTempFileWriter

---
+++ src/Writer/GzipFileWriter.php
@@ -0,0 +1,72 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * GpsLab component.
+ *
+ * @author    Peter Gribanov <info@peter-gribanov.ru>
+ * @copyright Copyright (c) 2011-2019, Peter Gribanov
+ * @license   http://opensource.org/licenses/MIT
+ */
+
+namespace GpsLab\Component\Sitemap\Writer;
+
+use GpsLab\Component\Sitemap\Writer\Exception\CompressionLevelException;
+use GpsLab\Component\Sitemap\Writer\Exception\ExtensionNotLoadedException;
+use GpsLab\Component\Sitemap\Writer\Exception\FileAccessException;
+
+class GzipFileWriter implements Writer
+{
+    /**
+     * @var resource|null
+     */
+    private $handle;
+
+    /**
+     * @var int
+     */
+    private $compression_level = 9;
+
+    /**
+     * @param int $compression_level
+     */
+    public function __construct(int $compression_level)
+    {
+        if ($compression_level < 1 || $compression_level > 9) {
+            throw CompressionLevelException::invalid($compression_level, 1, 9);
+        }
+
+        if (!extension_loaded('zlib')) {
+            throw ExtensionNotLoadedException::zlib();
+        }
+
+        $this->compression_level = $compression_level;
+    }
+
+    /**
+     * @param string $filename
+     */
+    public function open(string $filename): void
+    {
+        $mode = 'wb'.$this->compression_level;
+        $this->handle = @gzopen($filename, $mode);
+
+        if ($this->handle === false) {
+            throw FileAccessException::notWritable($filename);
+        }
+    }
+
+    /**
+     * @param string $content
+     */
+    public function write(string $content): void
+    {
+        gzwrite($this->handle, $content);
+    }
+
+    public function close(): void
+    {
+        gzclose($this->handle);
+        $this->handle = null;
+    }
+}
---
+++ tests/Writer/GzipTempFileWriterTest.php
@@ -0,0 +1,84 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * GpsLab component.
+ *
+ * @author    Peter Gribanov <info@peter-gribanov.ru>
+ * @copyright Copyright (c) 2011-2019, Peter Gribanov
+ * @license   http://opensource.org/licenses/MIT
+ */
+
+namespace GpsLab\Component\Sitemap\Tests\Writer;
+
+use GpsLab\Component\Sitemap\Writer\Exception\CompressionLevelException;
+use GpsLab\Component\Sitemap\Writer\GzipTempFileWriter;
+use PHPUnit\Framework\TestCase;
+
+class GzipTempFileWriterTest extends TestCase
+{
+    /**
+     * @var GzipTempFileWriter
+     */
+    private $writer;
+
+    /**
+     * @var string
+     */
+    private $filename;
+
+    protected function setUp(): void
+    {
+        if (!extension_loaded('zlib')) {
+            $this->markTestSkipped('The Zlib PHP extension is not loaded.');
+        }
+
+        $this->writer = new GzipTempFileWriter(9);
+        $this->filename = tempnam(sys_get_temp_dir(), 'sitemap');
+    }
+
+    protected function tearDown(): void
+    {
+        if (file_exists($this->filename)) {
+            unlink($this->filename);
+        }
+    }
+
+    /**
+     * @return array
+     */
+    public function getCompressionLevels(): array
+    {
+        return [
+            [0, false],
+            [-1, false],
+            [10, false],
+            [11, false],
+        ];
+    }
+
+    /**
+     * @dataProvider getCompressionLevels
+     *
+     * @param int  $compression_level
+     */
+    public function testInvalidCompressionLevel(int $compression_level): void
+    {
+        $this->expectException(CompressionLevelException::class);
+        new GzipTempFileWriter($compression_level);
+    }
+
+    public function testWrite(): void
+    {
+        $this->writer->open($this->filename);
+        $this->writer->write('foo');
+        $this->writer->write('bar');
+        $this->writer->close();
+
+        $handle = gzopen($this->filename, 'rb9');
+        $content = gzread($handle, 128);
+        gzclose($handle);
+
+        self::assertEquals('foobar', $content);
+    }
+}
