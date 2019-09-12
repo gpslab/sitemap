eef23f08798269ec94f538c67550e6e64b50f7ba
Peter Gribanov <wcode404@gmail.com>
2019-09-12 15:08:57 +0300

    fix CS

--- tests/Writer/DeflateTempFileWriterTest.php
+++ tests/Writer/DeflateTempFileWriterTest.php
@@ -22,8 +22,11 @@ use PHPUnit\Framework\TestCase;
 class DeflateTempFileWriterTest extends TestCase
 {
     private const ENCODINGS = [ZLIB_ENCODING_RAW, ZLIB_ENCODING_GZIP, ZLIB_ENCODING_DEFLATE];
+
     private const LEVELS = [-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
     private const MEMORIES = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
     private const WINDOWS = [8, 9, 10, 11, 12, 13, 14, 15];
 
     /**
